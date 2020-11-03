2bf5076ed762bf9f8d427f4907cd8e0584a246ba
coversocks <covertlysocks@gmail.com>
2020-11-03 23:10:42 +0800

    change to new handler.

--- src/include/tun2call/netif.h
+++ src/include/tun2call/netif.h
@@ -9,31 +9,25 @@ extern "C" {
 #include "lwip/pbuf.h"
 
 struct netif_handler;
-typedef void (*netif_handler_init_fn)(struct netif_handler* handler, struct netif* netif);
-typedef ssize_t (*netif_handler_read_fn)(struct netif_handler* handler);
-typedef ssize_t (*netif_handler_write_fn)(struct netif_handler* handler);
+typedef void (*netif_handler_init_fn)(struct netif_handler *handler,
+                                      struct netif *netif);
+typedef struct pbuf *(*netif_handler_read_fn)(struct netif_handler *handler);
+typedef ssize_t (*netif_handler_write_fn)(struct netif_handler *handler,
+                                          struct pbuf *p);
 
 struct netif_handler {
-  void* user;
+  void *user;
   ip4_addr_t ipaddr;
   ip4_addr_t netmask;
   ip4_addr_t gw;
-  char *rbuf;
-  u16_t rbuf_size;
-  u16_t rbuf_cap;
-  char *wbuf;
-  u16_t wbuf_size;
-  u16_t wbuf_cap;
   netif_handler_init_fn init;
   netif_handler_read_fn read;
   netif_handler_write_fn write;
   int enable_ipv6;
 };
-void netif_handler_set(struct netif_handler* handler,
-                       u32_t ipaddr,
-                       u32_t netmask,
-                       u32_t gw);
-void netif_default_init(struct netif_handler* handler);
+void netif_handler_set(struct netif_handler *handler, u32_t ipaddr,
+                       u32_t netmask, u32_t gw);
+void netif_default_init(struct netif_handler *handler);
 void netif_default_poll();
 void netif_default_free();
 
--- src/tun2call/netif.c
+++ src/tun2call/netif.c
@@ -25,29 +25,23 @@
 #if __ANDROID__
 
 #include <android/log.h>
-#define  LOG_TAG    "coversocks"
-#define  LOG_DEBUG(...)  __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
-#define  LOG_ERROR(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOG_TAG "coversocks"
+#define LOG_DEBUG(...)                                                         \
+  __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define LOG_ERROR(...)                                                         \
+  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
 #else
 
-#define  LOG_DEBUG(...)  printf(__VA_ARGS__)
-#define  LOG_ERROR(...)  perror(__VA_ARGS__)
+#define LOG_DEBUG(...) printf(__VA_ARGS__)
+#define LOG_ERROR(...) perror(__VA_ARGS__)
 
 #endif
 
-static err_t netif_default_output(struct netif* netif, struct pbuf* p) {
-  struct netif_handler* handler = (struct netif_handler*)netif->state;
-  if (p->tot_len > handler->wbuf_cap) {
-    MIB2_STATS_NETIF_INC(netif, ifoutdiscards);
-    LOG_ERROR("netif_default_output: packet too large");
-    return ERR_IF;
-  }
-  /* initiate transfer(); */
-  pbuf_copy_partial(p, handler->wbuf, p->tot_len, 0);
-  handler->wbuf_size=p->tot_len;
+static err_t netif_default_output(struct netif *netif, struct pbuf *p) {
+  struct netif_handler *handler = (struct netif_handler *)netif->state;
   /* signal that packet should be sent(); */
-  ssize_t written = handler->write(handler);
+  ssize_t written = handler->write(handler, p);
   if (written < p->tot_len) {
     MIB2_STATS_NETIF_INC(netif, ifoutdiscards);
     LOG_ERROR("netif_default_output: write");
@@ -58,29 +52,14 @@ static err_t netif_default_output(struct netif* netif, struct pbuf* p) {
   }
 }
 
-static struct pbuf* netif_default_read(struct netif* netif) {
-  struct netif_handler* handler = (struct netif_handler*)netif->state;
-  ssize_t readlen = handler->read(handler);
-  if (readlen < 1) {
-    return 0;
-  }
-  u16_t len = (u16_t)readlen;
-  MIB2_STATS_NETIF_ADD(netif, ifinoctets, len);
-  /* We allocate a pbuf chain of pbufs from the pool. */
-  struct pbuf* p = pbuf_alloc(PBUF_RAW, len, PBUF_POOL);
-  if (p != NULL) {
-    pbuf_take(p, handler->rbuf, len);
-    /* acknowledge that packet has been read(); */
-  } else {
-    /* drop packet(); */
-    MIB2_STATS_NETIF_INC(netif, ifindiscards);
-    LOG_ERROR("netif_default_input: could not allocate pbuf by %d\n", len);
-  }
+static struct pbuf *netif_default_read(struct netif *netif) {
+  struct netif_handler *handler = (struct netif_handler *)netif->state;
+  struct pbuf *p = handler->read(handler);
   return p;
 }
 
-static void netif_default_input(struct netif* netif) {
-  struct pbuf* p = netif_default_read(netif);
+static void netif_default_input(struct netif *netif) {
+  struct pbuf *p = netif_default_read(netif);
   if (p == NULL) {
     return;
   }
@@ -89,7 +68,7 @@ static void netif_default_input(struct netif* netif) {
   }
 }
 
-static err_t netif_default_low_init(struct netif* netif) {
+static err_t netif_default_low_init(struct netif *netif) {
   MIB2_INIT_NETIF(netif, snmp_ifType_other, 100000000);
   netif->name[0] = 't';
   netif->name[1] = 'p';
@@ -106,25 +85,25 @@ static err_t netif_default_low_init(struct netif* netif) {
   netif->hwaddr_len = 6;
   /* device capabilities */
   netif->flags = NETIF_FLAG_BROADCAST | NETIF_FLAG_ETHARP | NETIF_FLAG_IGMP;
-  struct netif_handler* handler = netif->state;
+  struct netif_handler *handler = netif->state;
   handler->init(handler, netif);
   netif_set_link_up(netif);
   return ERR_OK;
 }
 
-static struct netif *default_=0;
+static struct netif *default_ = 0;
 
 /* globales variables for netifs */
-static void netif_default_status_callback(struct netif* netif) {
+static void netif_default_status_callback(struct netif *netif) {
   if (netif_is_up(netif)) {
     LOG_DEBUG("Starting lwIP, status is UP, local interface IP is %s\n",
-           ip4addr_ntoa(netif_ip4_addr(netif)));
+              ip4addr_ntoa(netif_ip4_addr(netif)));
   } else {
     LOG_DEBUG("Starting lwIP, status is DOWN\n");
   }
 }
 
-static void netif_default_link_callback(struct netif* netif) {
+static void netif_default_link_callback(struct netif *netif) {
   if (netif_is_link_up(netif)) {
     LOG_DEBUG("Starting lwIP, link is UP\n");
   } else {
@@ -132,26 +111,24 @@ static void netif_default_link_callback(struct netif* netif) {
   }
 }
 
-void netif_handler_set(struct netif_handler* handler,
-                       u32_t ipaddr,
-                       u32_t netmask,
-                       u32_t gw) {
+void netif_handler_set(struct netif_handler *handler, u32_t ipaddr,
+                       u32_t netmask, u32_t gw) {
   handler->ipaddr.addr = PP_HTONL(ipaddr);
   handler->netmask.addr = PP_HTONL(netmask);
   handler->gw.addr = PP_HTONL(gw);
 }
 
 /* This function initializes all network interfaces */
-void netif_default_init(struct netif_handler* handler) {
+void netif_default_init(struct netif_handler *handler) {
   LOG_DEBUG("Starting lwIP, local interface IP is %s\n",
-         ip4addr_ntoa(&handler->ipaddr));
-  default_=malloc(sizeof(struct netif));
-  netif_add(default_, &handler->ipaddr, &handler->netmask, &handler->gw, handler,
-            netif_default_low_init, netif_input);
-  if(handler->enable_ipv6){
+            ip4addr_ntoa(&handler->ipaddr));
+  default_ = malloc(sizeof(struct netif));
+  netif_add(default_, &handler->ipaddr, &handler->netmask, &handler->gw,
+            handler, netif_default_low_init, netif_input);
+  if (handler->enable_ipv6) {
     netif_create_ip6_linklocal_address(default_, 1);
     printf("Starting lwIP, ip6 linklocal address is %s\n",
-        ip6addr_ntoa(netif_ip6_addr(default_, 0)));
+           ip6addr_ntoa(netif_ip6_addr(default_, 0)));
   }
   netif_set_status_callback(default_, netif_default_status_callback);
   netif_set_link_callback(default_, netif_default_link_callback);
@@ -171,11 +148,10 @@ void netif_default_free() {
   netif_set_down(default_);
   netif_remove(default_);
   free(default_);
-  default_=0;
+  default_ = 0;
 }
 
-void lwip_platform_assert(const char *msg, int line, const char *file)
-{
+void lwip_platform_assert(const char *msg, int line, const char *file) {
   LOG_ERROR("Assertion \"%s\" failed at line %d in %s\n", msg, line, file);
   fflush(NULL);
   abort();
